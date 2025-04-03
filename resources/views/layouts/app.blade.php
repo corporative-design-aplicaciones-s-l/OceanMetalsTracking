10d5346903023965b5345258e388e90822fb5634
Trollkopf <max.serratosa@gmail.com>
2025-04-03 12:31:23 +0200

    standalone

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,7 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet"
         href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.8.1/font/bootstrap-icons.min.css">
-        <link rel="manifest" href="/manifest.json" />
+        <link rel="manifest" href="../manifest.json" />
 
 
     <!-- Incluir Bootstrap JS y sus dependencias (Popper) -->
